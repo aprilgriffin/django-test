2ab49dd4d104ef8d3c2b82325374b971af64cb8c
April Griffin <41488292+aprilgriffin@users.noreply.github.com>
2022-09-08 20:18:18 -0400

    Update settings.py

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
